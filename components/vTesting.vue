5492e90db59338ec44be801c7622d7793cc27303
Alexandr-web <alexandr.yakovlev228@gmail.com>
2022-08-22 22:23:13 +0300

    изменил индикатор активной буквы

--- components/vTesting.vue
+++ components/vTesting.vue
@@ -10,26 +10,33 @@
       >
         <div class="testing-focus__inner">
           <vCursorIcon :classes="['testing-focus__icon']" />
-          <span class="testing-focus__text">Нажмите, чтобы начать</span>
+          <span class="testing-focus__text">Нажмите, чтобы начать. <bold v-if="res">Результат: {{ res }} с/м</bold></span>
         </div>
       </div>
-      <p
+      <div
         class="testing__text"
-        :class="{
-          'testing__text--disabled': !start
-        }"
+        :class="{ 'testing__text--disabled': !start }"
       >
+        <div
+          v-if="start"
+          class="caret"
+          :style="{
+            'left': `${caretData.x}px`,
+            'top': `${caretData.y}px`,
+            'height': `${caretData.height}px`,
+          }"
+        ></div>
         <span
           v-for="(lData, index) in text"
           :key="index"
+          ref="letter"
           class="testing__letter"
           :class="{
             'testing__letter--complete': lData.complete,
-            'testing__letter--failure': lData.failure,
-            'testing__letter--active': lData.active,
+            'testing__letter--failure': lData.failure
           }"
         >{{ lData.letter }}</span>
-      </p>
+      </div>
     </div>
   </div>
 </template>
@@ -48,6 +55,60 @@
       start: {
         type: Boolean,
         required: true,
+      },
+      res: {
+        type: Number,
+        required: true,
+      },
+      indexActiveLetter: {
+        type: Number,
+        required: true,
+      },
+    },
+    data: () => ({
+      caretData: {
+        x: 0,
+        y: 0,
+        height: 0,
+      },
+    }),
+    computed: {
+      getActiveLetter() {
+        return this.$refs.letter[this.indexActiveLetter];
+      },
+    },
+    watch: {
+      start(val) {
+        if (val) {
+          this.moveCaret();
+
+          window.addEventListener("resize", this.moveCaret);
+        } else {
+          this.clearCaretData();
+
+          window.removeEventListener("resize", this.moveCaret);
+        }
+      },
+      indexActiveLetter() {
+        this.moveCaret();
+      },
+    },
+    methods: {
+      clearCaretData() {
+        this.caretData = {
+          x: 0,
+          y: 0,
+          height: 0,
+        };
+      },
+      moveCaret() {
+        const { offsetLeft, offsetTop, offsetHeight, } = this.getActiveLetter;
+
+        this.caretData = {
+          x: offsetLeft,
+          y: offsetTop,
+          height: offsetHeight,
+        };
       },
     },
   };
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,8 @@
           v-if="text && text.length"
           :text="text"
           :start="start"
+          :res="res"
+          :index-active-letter="indexActiveLetter"
           @startWriting="start = true"
         />
       </div>
@@ -25,25 +27,49 @@
         const token = store.getters["auth/getToken"];
         const { ok, text: textData, } = await store.dispatch("text/getRandom", token);
         const text = ok ? textData.body.split("").map((l, i) => ({ letter: l, active: i === 0, complete: false, failure: false, })) : [];
+        const totalWords = ok ? textData.body.split(" ").length : 0;
 
-        return { text: ok ? text : "", };
+        return {
+          text,
+          totalWords,
+        };
       } catch (err) {
         throw err;
       }
     },
     data: () => ({
       start: false,
+      res: 0,
+      timer: null,
+      sec: 0,
       invalidKeys: [
         "Escape", "Tab", "Alt", "Control", "Enter",
         "Shift", "Backspace", "NumLock", "Delete", "Insert",
         "CapsLock", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown",
-        "OS", "Meta"
+        "OS", "Meta", "F1", "F2", "F3", "F4", "F5", "F6",
+        "F7", "F8", "F9", "F10", "F11", "F12", "End",
+        "PageUp", "PageDown", "ScrollLock", "Pause", "Insert",
+        "Home"
       ],
+      indexActiveLetter: 0,
     }),
     head: { title: "Тестирование", },
     watch: {
       start(val) {
-        window[val ? "addEventListener" : "removeEventListener"]("keydown", this.keydownHandler);
+        if (val) {
+          this.timer = setInterval(() => {
+            this.sec += 1;
+          }, 1000);
+
+          window.addEventListener("keydown", this.keydownHandler);
+        } else {
+          window.removeEventListener("keydown", this.keydownHandler);
+
+          clearInterval(this.timer);
+
+          this.res = Math.floor(((this.totalWords || 1) / (this.sec || 1)) * 60);
+          this.sec = 0;
+        }
       },
     },
     methods: {
@@ -62,9 +88,9 @@
       },
       keydownHandler(e) {
         e.preventDefault();
-
+        
         const key = e.key;
-
+        
         if (!this.invalidKeys.includes(key)) {
           const indexActiveLetter = this.text.findIndex(({ active, }) => active);
           const activeLetter = this.text[indexActiveLetter];
@@ -76,6 +102,7 @@
             });
 
             if (indexActiveLetter + 1 >= this.text.length) {
+              this.indexActiveLetter = 0;
               this.start = false;
               this.text = this.text.map((item, index) => {
                 item.active = index === 0;
@@ -83,6 +110,8 @@
 
                 return item;
               });
+            } else {
+              this.indexActiveLetter = indexActiveLetter + 1;
             }
           } else {
             this.changeLetter({
