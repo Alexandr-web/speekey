5ffbdc62fd20054f5d756421b1081126e7ca6d02
Alexandr-web <alexandr.yakovlev228@gmail.com>
2022-09-19 13:50:58 +0300

    рефакторинг

--- __tests__/api/index.test.js
+++ __tests__/api/index.test.js
@@ -2,56 +2,34 @@ import { expect, test, describe, } from "@jest/globals";
 import { getFavorites, getCompletedTexts, } from "./functions/user/index";
 import { getRandom, getOne, getNext, } from "./functions/text/index";
 
+const requestHandler = (reqData) => {
+  reqData.map(({ promise, method, }) => {
+    promise.then(({ status, }) => {
+      expect(status)[method.name](...method.args);
+    });
+  });
+};
+
 describe("Profile api", () => {
   test("Get completed texts", () => {
-    const reqData = getCompletedTexts();
-
-    reqData.map(({ promise, method, }) => {
-      promise.then(({ status, }) => {
-        expect(status)[method.name](...method.args);
-      });
-    });
+    requestHandler(getCompletedTexts());
   });
 
   test("Get favorites", () => {
-    const reqData = getFavorites();
-
-    reqData.map(({ promise, method, }) => {
-      promise.then(({ status, }) => {
-        expect(status)[method.name](...method.args);
-      });
-    });
+    requestHandler(getFavorites());
   });
 });
 
 describe("Text api", () => {
   test("Get random", () => {
-    const reqData = getRandom();
-
-    reqData.map(({ promise, method, }) => {
-      promise.then(({ status, }) => {
-        expect(status)[method.name](...method.args);
-      });
-    });
+    requestHandler(getRandom());
   });
 
   test("Get one", () => {
-    const reqData = getOne();
-
-    reqData.map(({ promise, method, }) => {
-      promise.then(({ status, }) => {
-        expect(status)[method.name](...method.args);
-      });
-    });
+    requestHandler(getOne());
   });
 
   test("Get next", () => {
-    const reqData = getNext();
-
-    reqData.map(({ promise, method, }) => {
-      promise.then(({ status, }) => {
-        expect(status)[method.name](...method.args);
-      });
-    });
+    requestHandler(getNext());
   });
 });
